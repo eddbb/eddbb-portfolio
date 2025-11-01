3e3a8749e285298ae860293073cfed5136bf54b7
eddbb <edvin.blomgren@hotmail.com>
2025-11-01 20:44:37 +0100

    feat(projects): added course-compass and ea-seed

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "@/components/ProjectCard";
 export default function ProjectsPage() {
   return (
     <main>
-        <div className="flex flex-col items-center gap-6 mt-6">
+        <div className="flex flex-col items-center gap-6 mt-6 pb-10">
             <h1 className="text-4xl">My Projects</h1>
             <div className="h-0.5 bg-foreground w-72 rounded-full"></div>
             <p className="text-center max-w-xl">
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -7,8 +7,11 @@ export default function ProjectCard({ project }: { project: any }) {
   return (
     <Link href={`/projects/${project.slug}`}>
       <div className="w-lg rounded-2xl overflow-hidden hover:shadow-[0_4px_6px_1px_var(--middleground),0_2px_4px_1px_var(--middleground)] transition relative">
-        
-        <img src={project.image_header} alt={project.title} className="w-full h-48 object-cover" />
+        <img
+          src={project.image_header}
+          alt={project.title}
+          className="w-full h-48 object-cover"
+        />
 
         <div className="absolute top-2 left-2 bg-foreground rounded-md text-background">
           <h2 className="text-sm font-bold p-1">{project.status}</h2>
@@ -17,7 +20,9 @@ export default function ProjectCard({ project }: { project: any }) {
         <div className="p-4 bg-foreground text-background rounded-t-lg -mt-6 relative z-10">
           <h3 className="text-xl mb-2">{project.title}</h3>
           <p className="text-sm mb-2">{project.short_description}</p>
-          <div className="text-sm text-(--middleground-2)">{project.tech.join(", ")}</div>
+          <div className="text-sm text-(--middleground-2)">
+            {project.tech.slice(0, 3).join(", ")} and more...
+          </div>
         </div>
       </div>
     </Link>
